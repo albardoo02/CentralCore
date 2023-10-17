217a3b9bcf6c4834c8fcc25d20f1744392e579ab
albardoo02 <albardoo02@gmail.com>
2023-10-17 14:00:40 +0900

    イベントの記述ミス修正

--- src/main/java/com/github/albardoo02/Listeners/BlockBreak.java
+++ src/main/java/com/github/albardoo02/Listeners/BlockBreak.java
@@ -1,17 +1,16 @@
 package com.github.albardoo02.Listeners;
 
 import com.github.albardoo02.CentralCore;
-import com.github.albardoo02.ItemManager;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import net.milkbowl.vault.economy.Economy;
 import net.milkbowl.vault.economy.EconomyResponse;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.BlockBreakEvent;
 
 public class BlockBreak implements Listener {
 
@@ -22,20 +21,15 @@ public class BlockBreak implements Listener {
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
-    public void onBlockBreak(PlayerInteractEvent event) {
+    public void onPlayerInteract(BlockBreakEvent event) {
         Economy economy = CentralCore.getEconomy();
         Player player = event.getPlayer();
-        Action action = event.getAction();
+        Block block = event.getBlock();
         EconomyResponse response = economy.depositPlayer(player, Double.parseDouble(plugin.getConfig().getString("BlockBreakMoney.Amount")));
         if (!plugin.getConfig().getBoolean("BlockBreakMoney.Enabled")) {
             return;
         }
-        if (event.getItem() != null) {
-            if (event.getItem().getItemMeta().equals((ItemManager.stick.getItemMeta()))) {
-                return;
-            }
-        }
-        if(action.equals(Action.LEFT_CLICK_BLOCK)){
+        if(block.getType() == event.getBlock().getType()){
             if (response.transactionSuccess()) {
                 player.spigot().sendMessage(
                         ChatMessageType.ACTION_BAR,
